70dc8646bf6a8ba08a94e004ce3daac3e2068dbd
Tarhun <TarhunchicKKK@yandex.by>
2024-05-16 18:36:15 +0300

    Some change in file transfering

--- file-transfer/src/files/files.controller.ts
+++ file-transfer/src/files/files.controller.ts
@@ -1,24 +1,34 @@
-import { Controller, Get, Injectable, Post, StreamableFile, UploadedFile, UseInterceptors } from "@nestjs/common";
+import {
+    Controller,
+    Get,
+    Injectable,
+    Post,
+    StreamableFile,
+    UploadedFile,
+    UseInterceptors,
+} from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
 import { createReadStream, createWriteStream } from "fs";
 import { join } from "path";
 
-@Controller('files')
+@Controller("files")
 export class FilesController {
     @Get()
     public download(): StreamableFile {
-        const file = createReadStream(join(process.cwd(), 'package.json'))
-        return new StreamableFile(file)
+        const file = createReadStream(join(process.cwd(), "package.json"));
+        return new StreamableFile(file);
     }
 
     @Post()
-    @UseInterceptors(FileInterceptor('file'))
+    @UseInterceptors(FileInterceptor("file"))
     upload(@UploadedFile() file: Express.Multer.File) {
-        const fileStream = createWriteStream(join(process.cwd(), `src/files/${file.originalname}`))
-        fileStream.write(file.buffer)
-        fileStream.close()
+        const fileStream = createWriteStream(
+            join(process.cwd(), `src/files/${file.originalname}`)
+        );
+        fileStream.write(file.buffer);
+        fileStream.close();
 
-        console.log(file.buffer)
-        console.log(file)
+        console.log(file.buffer);
+        console.log(file);
     }
 }
